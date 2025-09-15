de8fe12705a8d0c1b29cb2c3aa38f30f57ed070e
Sean Delos Santos <sdelos-santos1@myseneca.ca>
2025-09-15 10:12:23 -0400

    Redid sign up form validation with own logic

--- main/app.js
+++ main/app.js
@@ -1,54 +1,45 @@
-const form = document.getElementById('form')
-const emailInput = document.getElementById('email')
-const passwordInput = document.getElementById('password')
-const firstNameInput = document.getElementById('first-name')
-const lastNameInput = document.getElementById('last-name')
-const contactInput = document.getElementById('contact')
-const addressInput = document.getElementById('address')
-const errorMessage = document.getElementById('error-message')
+const form = document.querySelector(".auth-form")
+const submit = document.querySelector(".btn")
+const email = document.querySelector("#email")
+const password = document.querySelector("#password")
+const firstName = document.querySelector("#first-name")
+const lastName = document.querySelector("#last-name")
+const contact = document.querySelector("#contact")
+const address = document.querySelector("#address")
 
-form.addEventListener('submit', (e) => {
-    let errors = []
+submit.addEventListener("click", (e) => {
+    e.preventDefault()
 
-    if(firstNameInput) {
-        errors = getRegisterFormErrors(emailInput.value, passwordInput.value, firstNameInput.value, lastNameInput.value, contactInput.value, addressInput.value)
+    let messages = []
+
+    if(messages = validateInputs()) {
+        // function verified - show verified UX
+
+        return // does not create error box if no error
     } else {
-        errors = getLoginFormErrors(emailInput.value, passwordInput.value) 
-    }
-
-    if (errors.length > 0) {
-        e.preventDefault()
-        errorMessage.innerText = errors.join(". ")
+        createErrorBox(messages)
     }
 })
 
-function getRegisterFormErrors(email, password, firstName, lastName, contact, address) {
-    let errors = []
-
-    if (email === '' || email === null) {
-        errors.push('Email is empty')
-        emailInput.parentElement.classList.add('incorrect')
-    }
-    if (password === '' || password === null) {
-        errors.push('Password is empty')
-        passwordInput.parentElement.classList.add('incorrect')
-    }
-    if (firstName === '' || firstName === null) {
-        errors.push('First name is empty')
-        firstNameInput.parentElement.classList.add('incorrect')
-    }
-    if (lastName === '' || lastName === null) {
-        errors.push('Last name is empty')
-        lastNameInput.parentElement.classList.add('incorrect')
-    }
-    if (contact === '' || contact === null) {
-        errors.push('Contact is empty')
-        contactInput.parentElement.classList.add('incorrect')
-    }
-    if (address === '' || address === null) {
-        errors.push('Address is empty')
-        addressInput.parentElement.classList.add('incorrect')
-    }
-
-    return errors
+// validates inputs
+function validateInputs() {
+    // add input logic
 }
+
+// creates error box
+function createErrorBox(messages) {
+    const errorBox = document.createElement('div')
+    Object.assign(errorBox.style, {
+        display : "block",
+        padding : "1em",
+        width : "100%",
+        background : "white"
+    })
+
+    const errorBoxText = document.createElement('span')
+    errorBoxText.textContent = messages
+    errorBox.append(errorBoxText)
+
+    form.append(errorBox)
+}
+
