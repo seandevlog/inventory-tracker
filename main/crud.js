e3afbab6c2b5f0d1b7210e08e2da4b919a5fb74e
Sean Delos Santos <sdelos-santos1@myseneca.ca>
2025-09-22 01:28:36 -0400

    interaction for closing and opening of modal

--- main/crud.js
+++ main/crud.js
@@ -1,15 +1,37 @@
 const tableBody = document.getElementById("crud-table-body")
 const addUser = document.getElementById("add-user")
+const editUser = document.querySelectorAll(".edit-btn")
+const modalWrapper = document.getElementById("modal-wrapper")
+const modal = document.getElementById("modal")
 
 addUser.addEventListener('click', (e) => {
     e.preventDefault()
 
-    const newRow = tableBody.insertRow()
-    
-    newRow.insertCell().textContent = "123"
-    newRow.insertCell().textContent = "123"
+    // const newRow = tableBody.insertRow()
+    // newRow.insertCell().textContent = "123"
+    // newRow.insertCell().textContent = "123"
 
     // add function to parse data from temporary stored object
     // then make a modal function to store an object
-    
+    modalWrapper.style.display = "flex"
+
+    modal.querySelector("header h1").innerText = "Add User"
+})
+
+editUser.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        e.preventDefault()
+
+        modalWrapper.style.display = "flex"
+
+        modal.querySelector("header h1").innerText = "Edit User"
+    })
+})
+
+modalWrapper.addEventListener('click', (e) => {
+    e.preventDefault()
+
+    if (e.target === e.currentTarget) {
+        modalWrapper.style.display = "none" 
+    }
 })
