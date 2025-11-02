0ed30ad36e24611646aeb49c2bf8a2d6141c8763
Sean Delos Santos <sdelos-santos1@myseneca.ca>
2025-11-02 11:25:49 -0500

    completed and optimized vanillaJS for auth page

--- main/auth.js
+++ main/auth.js
@@ -5,17 +5,57 @@
 init();
 
 function init() {
-    doLoginForm('#auth>form');
-    // if user clicks "auth redirect" then redirects to signup/login
-    
+    toggleAuthForm();
 }
 
-// this function should be able to change the elements inside the form
-function toggleForm(text, form) {
+function toggleAuthForm(string = 'login') {
+    const form = document.querySelector('div#auth>form');
+    const button = form.elements['button'];
+    const inputsNotInLogin = document.querySelectorAll('div#auth>form input:not(input[name="email"],input[name="password"]');
+    const redirect = document.querySelector('div#auth>span#redirect');
+    const title = document.querySelector('head>title');
+
+    if (string === 'login') {
+        title.innerText = 'Login';
+
+        for (const input of inputsNotInLogin) {
+            const label = input.previousElementSibling;
+            input.classList.add('hide');
+            label.classList.add('hide');
+        }
+
+        button.innerText = 'Login';
+
+        redirect.innerText = 'I don\'t have an account';
+
+        doLoginForm(form);
+
+    } else if (string === 'signup') {
+        title.innerText = 'Sign Up';
+
+        for (const input of inputsNotInLogin) {
+            const label = input.previousElementSibling;
+            input.classList.remove('hide');
+            label.classList.remove('hide');
+        }
+
+        button.innerText = 'Sign Up';
+
+        redirect.innerText = 'I already have an account';
+
+        doSignUpForm(form);
+    }
+
+    redirect.onclick = e => {
+        if (string === 'login') {
+            toggleAuthForm('signup');
+        } else if (string === 'signup') {
+            toggleAuthForm('login');
+        }
+    };
 }
 
-function doLoginForm(formId) {
-    const form = document.querySelector(formId);
+function doLoginForm(form) {
     const email = form.elements['email'];
     const password = form.elements['password'];
     const button = form.elements['button'];
@@ -37,8 +77,7 @@ function doLoginForm(formId) {
     }
 }
 
-function doSignUpForm(formId) {
-    const form = document.querySelector(formId);
+function doSignUpForm(form) {
     const email = form.elements['email'];
     const password = form.elements['password'];
     const firstName = form.elements['first-name'];
