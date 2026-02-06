9ad5726d8615ce182bb27c034ded9cf336c2a900
unknown <seandevlog@proton.me>
2026-02-06 14:22:53 -0500

    Responsive 'create' button with a hover reveal description

--- frontend/src/components/buttons/create/create.jsx
+++ frontend/src/components/buttons/create/create.jsx
@@ -1,26 +1,22 @@
-import { useState } from 'react';
 import styles from './create.module.css';
 import Plus from '@assets/plus.svg';
 
 const Create = ({ children, onClick }) => {
-  const [showDescription, setShowDescription] = useState(false);
 
   return (
     <div 
       className={styles.create}
-      onMouseOver={() => setShowDescription(true)}
-      onMouseOut={() => setShowDescription(false)}
     >
-      { showDescription &&
+      <div>
         <div className={styles.description}>
           {children}
         </div>
-      }
-      <button
-        onClick={onClick}
-      >
-        <Plus/>
-      </button>
+        <button
+          onClick={onClick}
+        >
+          <Plus/>
+        </button>
+      </div>
     </div>
   )
 }
