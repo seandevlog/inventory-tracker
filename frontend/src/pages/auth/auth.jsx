dc258a3046fc240cce71c269f5e47cfe8c254d70
unknown <seandevlog@proton.me>
2026-02-18 17:51:10 -0500

    Updated auth loader flow

--- frontend/src/pages/auth/auth.jsx
+++ frontend/src/pages/auth/auth.jsx
@@ -1,7 +1,20 @@
-import { Outlet } from "react-router-dom";
+import { useContext, useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
 import styles from './auth.module.css';
 
+import AppContext from '@contexts/app.context';
+
+import config from '@config';
+const { path } = config;
+
 const Auth = () => {
+  const { token } = useContext(AppContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (token) navigate(path.root);
+  }, [token, navigate]);
+  
   return (
     <div className={styles.wrapper}>
       <div className={styles.auth}>
